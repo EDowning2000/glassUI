2a77247e68a6a23e62e0c1555f56c3abfc76adba
Emerson Downing <emersondowning@gmail.com>
2021-01-23 16:34:43 -0500

    got the images rendering as well as the containers

--- client/src/components/Glass.js
+++ client/src/components/Glass.js
@@ -1,5 +1,14 @@
 import React from "react"
 import "./glass.css"
+import assassins from "../images/assassins.png"
+import avatar from "../images/avatar.png"
+import controller from "../images/controller.png"
+import library from "../images/library.png"
+import sackboy from "../images/sackboy.png"
+import spiderman from "../images/spiderman.png"
+import steam from "../images/steam.png"
+import twitch from "../images/twitch.png"
+import upcoming from "../images/upcoming.png"
 
 const Glass = () => {
   return (
@@ -8,8 +17,29 @@ const Glass = () => {
         <section className="glass">
           <div className="dashboard">
             <div className="user">
-              <img src="./images/avatar.png" alt="User Image" />
+              <img src={avatar} alt="User" />
               <h3>Emerson Downing</h3>
+              <p>Pro Member</p>
+            </div>
+            <div className="links">
+              <div className="link">
+                <img src={twitch} alt="Stream" />
+                <h2>Streams</h2>
+              </div>
+
+              <div className="link">
+                <img src={steam} alt="Games" />
+                <h2>Games</h2>
+              </div>
+
+              <div className="link">
+                <img src={upcoming} alt="New" />
+                <h2>New</h2>
+              </div>
+              <div className="link">
+                <img src={library} alt="Library" />
+                <h2>Library</h2>
+              </div>
             </div>
           </div>
         </section>
